2a2d704786e79e267c794c92e1036455cde0f774
Kirill Maksimenko <119590926+MPS036@users.noreply.github.com>
2026-02-19 15:43:28 +0100

    Update buttons.py

--- Password-Generator/buttons.py
+++ Password-Generator/buttons.py
@@ -1,20 +1,28 @@
+"""
+UI button configuration and character sets.
+"""
+
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from enum import Enum
 
 
 class Characters(Enum):
+    """
+    Character groups that can be enabled in the password generator UI.
+    """
     btn_lower = ascii_lowercase
     btn_upper = ascii_uppercase
     btn_digits = digits
     btn_special = punctuation
 
+
 CHARACTER_NUMBER = {
-    'btn_lower': len(Characters.btn_lower.value),
-    'btn_upper': len(Characters.btn_upper.value),
-    'btn_digits': len(Characters.btn_digits.value),
-    'btn_special': len(Characters.btn_special.value)
+    "btn_lower": len(Characters.btn_lower.value),
+    "btn_upper": len(Characters.btn_upper.value),
+    "btn_digits": len(Characters.btn_digits.value),
+    "btn_special": len(Characters.btn_special.value),
 }
 
 GENERATE_PASSWORD = (
-    'btn_lower', 'btn_upper', 'btn_digits', 'btn_special', 'btn_refresh'
+    "btn_lower", "btn_upper", "btn_digits", "btn_special", "btn_refresh"
 )
